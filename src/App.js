662eec4e66f2e391990fa197a50a0612012198b0
John Panton <johnpanton29@outlook.com>
2023-01-17 08:59:04 +0000

    fixed bug

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
       const copyOfList=[...todoList,newTaskObj]
       setTodoList(copyOfList)
       setTaskName("")
+      setTaskPriority("")
       resetForm()
     }else{
       event.preventDefault();
